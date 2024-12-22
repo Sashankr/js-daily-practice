ad55801f67d70ed9000191dc73a840e4c5f03d0c
Sashankr <sashankr97@gmail.com>
2024-12-22 12:35:41 +0530

    aggregation, segregation with reduce

--- arrayReduce.js
+++ arrayReduce.js
@@ -1,28 +1,28 @@
-/*
-Array reduce accepts an initial value, it starts with initial value and then the returned value from the last call in same 
-function 
-Syntax
-
-arr.reduce((prevValue,currentValue,currentIndex,array)=> {
-        const next = prevValue + currentValue;
-        return next
-    })
-
-    prevValue - the value returned from last call of function or initial value in first iteration
-    currentValue - current value of array.
-    currentIndex - current index of array.
-    array - the array itself
-
-*/
-
 // Aggregation
-// sum of all numbers in array
 
-let arr = [1, 2, 3, 4, 5, 6];
+const arr = [1, 2, 3, 4, 5, 6];
 
-const result = arr.reduce((prev, current, index, arr) => {
+const result = arr.reduce((prev, current, currentIndex, arr) => {
   const next = prev + current;
   return next;
 }, 0);
 
 console.log(result);
+
+// Segregation
+
+const arr2 = [1.1, 1.2, 1.3, 2.1, 2.2, 2.3];
+
+const segregatedList = arr2.reduce((prev, current) => {
+  const floor = Math.floor(current);
+  console.log("floor", floor);
+  console.log("prev", prev);
+
+  if (!prev[floor]) {
+    prev[floor] = [];
+  }
+  prev[floor].push(current);
+  return prev;
+}, {});
+
+console.log(segregatedList);
